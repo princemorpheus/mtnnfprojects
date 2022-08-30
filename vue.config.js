47d058ca6797bc1245a877a3cb44fd5ae0fe8dca
Prince Morpheus <princemorpheus@gmail.com>
2022-08-30 14:08:22 +0100

    Updated

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack')
+// const path = require("path");
+
+// module.exports = {
+//   outputDir: path.resolve(__dirname, "../backend/templates/SPA"),
+//   assetsDir: "../../static/SPA"
+// }
+module.exports = {
+  configureWebpack: {},
+  publicPath: process.env.NODE_ENV === 'production' ? '/mtnnfprojects/' : '/'
+  // publicPath:
+  //   process.env.NODE_ENV === 'production'
+  //     ? '/mtn/mtnf-map/wp-content/plugins/mtnnf-map/templates/mtnnfmap/dist/'
+  //     : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //     ? '/mtnnfmap/'
+  //     : '/'
+}
